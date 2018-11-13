779d24134ded24edac1ce1404b465cef5486fb31
Jonas <jwallat@hotmail.de>
2018-11-13 17:56:05 +0100

    implemented (single global instance) BLOC pattern for the login screen

---
+++ lib/src/blocs/bloc.dart
@@ -0,0 +1,23 @@
+import "dart:async";
+import "validators.dart";
+
+class Bloc with Validators {
+  final _emailController = StreamController<String>();
+  final _passwordController = StreamController<String>();
+
+  // Add data to stream
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  // Retrieve data from stream
+  Stream<String> get email => _emailController.stream.transform(validateEmail);
+  Stream<String> get password =>
+      _passwordController.stream.transform(validatePassword);
+
+  void dispose() {
+    _emailController.close();
+    _passwordController.close();
+  }
+}
+
+final bloc = Bloc();
--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,12 +1,64 @@
 import "package:flutter/material.dart";
+import "../blocs/bloc.dart";
 
 class LoginScreen extends StatelessWidget {
   Widget build(context) {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container(
+            margin: EdgeInsets.only(
+              top: 25.0,
+            ),
+          ),
+          submitButton(),
+        ],
       ),
+    );
+  }
+
+  Widget emailField() {
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: "you@example.com",
+            labelText: "Email Address",
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget passwordField() {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changePassword,
+          decoration: InputDecoration(
+            hintText: "Password",
+            labelText: "Password",
+            errorText: snapshot.error,
+          ),
+          obscureText: true,
+        );
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text("login"),
+      color: Colors.blue,
+      onPressed: () => {},
     );
   }
 }
